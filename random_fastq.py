74d4f8b57b1a75cbe9afcf9f0cf4a69307df636f
iyer.laxman4@gmail.com <iyer.laxman4@gmail.com>
2018-04-22 16:02:47 +0530

    first

---
+++ random_fastq.py
@@ -0,0 +1,33 @@
+import random,sys,re
+seq=''
+random_base_insert=['A','T','G','C']
+fp=open(sys.argv[1],'r')
+for recs in fp:
+    recs=recs.strip('\n')
+    pattern=re.search('^>',recs)
+    if pattern:
+        next
+    else:
+        seq+=recs
+fp.close()
+quality=''
+for i in range(0,50):
+    quality+='1'
+header='@HWWI:1:FCX:1:15:6329:1045 1:N:0:2'
+for x in range(100000):
+    read=''
+    val=random.randint(0,len(seq)-50)
+    
+    for y in range(0,50):
+        if x%2!=0:
+            
+            val2=random.randint(0,50)
+            if int(y)==int(val2):
+                read+=random_base_insert[random.randint(0,3)]
+                
+           
+            else:
+                read+=seq[val+y]
+        else:
+            read+=seq[val+y] 
+    print(header+'\n'+read+'\n+\n'+quality)
---
+++ readme.md
@@ -0,0 +1,4 @@
+Picked random sequences from chrM&22.
+Random error rates were 1 in 100.
+Then aligned to chr21.
+The alignment % is 11%
